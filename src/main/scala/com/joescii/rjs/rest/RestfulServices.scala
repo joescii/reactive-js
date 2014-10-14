a4c834a81cfe0041b46a7d8766a5f17d948f95ce
Joe Barnes <barnesjd@gmail.com>
2014-10-13 22:09:25 -0500

    Updated restful service to actually use the symbol argument

--- src/main/scala/com/joescii/rjs/rest/RestfulServices.scala
+++ src/main/scala/com/joescii/rjs/rest/RestfulServices.scala
@@ -16,17 +16,12 @@ object RestfulServices extends RestHelper {
 
   serve {
     case "quote" :: symbol :: Nil Get _ => {
-      val f:Future[LiftResponse] = Http(url("http://download.finance.yahoo.com/d/quotes.csv?s=MENT&f=l1").GET OK as.String).either.map(_ match {
+      val f:Future[LiftResponse] = Http(url(s"http://download.finance.yahoo.com/d/quotes.csv?s=$symbol&f=l1").GET OK as.String).either.map(_ match {
         case Right(quote) => PlainTextResponse(quote, List(), 200)
         case Left(err) => err.printStackTrace(); ServiceUnavailableResponse(1000)
       })
 
-//      Http(url("http://download.finance.yahoo.com/d/quotes.csv?s=MENT&f=l1").GET).map(res =>
-//        println(res.getHeaders)
-//      )
-
       val b = Full(Await.result(f, 5.seconds))
-//      val b = Full(ServiceUnavailableResponse(1000))
       b
     }
   }
