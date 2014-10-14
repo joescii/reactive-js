cdbd1704b769c209b1c83ee90d61953b631146a1
Joe Barnes <barnesjd@gmail.com>
2014-10-14 18:53:48 -0500

    Added currency exchange services

--- src/main/scala/com/joescii/rjs/rest/RestfulServices.scala
+++ src/main/scala/com/joescii/rjs/rest/RestfulServices.scala
@@ -14,15 +14,25 @@ object RestfulServices extends RestHelper {
     LiftRules.statelessDispatch.append(RestfulServices)
   }
 
-  serve {
-    case "quote" :: symbol :: Nil Get _ => {
-      val f:Future[LiftResponse] = Http(url(s"http://download.finance.yahoo.com/d/quotes.csv?s=$symbol&f=l1").GET OK as.String).either.map(_ match {
-        case Right(quote) => PlainTextResponse(quote, List(), 200)
-        case Left(err) => err.printStackTrace(); ServiceUnavailableResponse(1000)
-      })
+  def response(params:String, success: String => String = {s => s} ) = {
+    val f:Future[LiftResponse] = Http(url(s"http://download.finance.yahoo.com/d/quotes.csv?$params").GET OK as.String).either.map(_ match {
+      case Right(quote) => PlainTextResponse(success(quote))
+      case Left(err) => err.printStackTrace(); ServiceUnavailableResponse(1000)
+    })
 
-      val b = Full(Await.result(f, 5.seconds))
-      b
-    }
+    val b = Full(Await.result(f, 5.seconds))
+    b
+  }
+
+  def convert(quote:String, amount:String):String = {
+    val rate = quote.trim.toFloat
+    val amt = amount.trim.toFloat
+    (rate * amt).toString
+  }
+
+  serve {
+    case "quote" :: symbol :: Nil Get _ => response(s"s=$symbol&f=l1" )
+    case "exchange" :: currency :: Nil Get _ => response(s"s=USD$currency=X&f=l1" )
+    case "convert" :: currency :: amount :: Nil Get _ => response(s"s=USD$currency=X&f=l1", quote => convert(quote, amount) )
   }
 }
