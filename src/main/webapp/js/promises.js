3fd73c3e45c34a98ff7f7146d87679bb60b58e8c
Joe Barnes <barnesjd@gmail.com>
2014-10-14 19:02:36 -0500

    Added conversion to EUR

--- src/main/webapp/js/promises.js
+++ src/main/webapp/js/promises.js
@@ -1,12 +1,23 @@
 angular.module('Promises', ['DemoServices'])
 
 .controller('PromisesSlide', ['$scope', 'callbackHttp', function($scope, http){
-  var success = function(quote) {
-    $scope.ment = quote;
+  $scope.usd = 0;
+  $scope.eur = 0;
+
+  var withQuote = function(quote) {
+    $scope.usd = quote;
+    convert(quote);
+  };
+  var withConversion = function(converted) {
+    $scope.eur = converted;
+  };
+
+  var convert = function(usd) {
+    http.get("/convert/EUR/"+usd, withConversion);
   };
 
   $scope.onQuote = function() {
-    http.get("/quote/MENT", success);
+    http.get("/quote/MENT", withQuote);
   }
 
 }])
