c90e70b21158cd61404264a248498f1a9c63e970
Joe Barnes <barnesjd@gmail.com>
2014-10-14 20:06:50 -0500

    Now using a select box for conversions

--- src/main/webapp/js/promises.js
+++ src/main/webapp/js/promises.js
@@ -1,29 +1,25 @@
 angular.module('Promises', ['DemoServices'])
 
 .controller('PromisesSlide', ['$scope', '$http', function($scope, http){
-  $scope.usd = 0;
-  $scope.eur = 0;
-
-  var withQuote = function(quote) {
-    $scope.usd = quote;
-    return convert(quote);
-  };
-  var withConversion = function(converted) {
-    $scope.eur = converted;
-  };
-
-  var convert = function(usd) {
-    return http.get("/convert/EUR/"+usd);
-  };
+  $scope.graphics = ['£', '€', '\u20B9', '$'];
+  $scope.currencies = ['GBP', 'EUR', 'INR', 'CLP'];
+  $scope.currency = 0;
+  $scope.graphic = $scope.graphics[0];
+  $scope.quote = 0;
 
   var quote = function() {
     return http.get("/quote/MENT");
   };
 
+  var convert = function(usd) {
+    return http.get("/convert/"+$scope.currencies[$scope.currency]+"/"+usd);
+  };
+
   $scope.onQuote = function() {
+    $scope.graphic = $scope.graphics[$scope.currency];
     quote()
-      .then(function(usd){return withQuote(usd.data)})
-      .then(function(eur){return withConversion(eur.data)})
+      .then(function(usd){return convert(usd.data)})
+      .then(function(price){$scope.quote = price.data})
       .catch(function(err){console.log("You can't get ye stocks!!!")})
   }
 
