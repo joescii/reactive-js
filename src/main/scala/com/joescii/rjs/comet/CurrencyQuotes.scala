7d9d33c8236163718c42db3d8028632a81a4a61e
Joe Barnes <barnesjd@gmail.com>
2014-10-16 21:46:43 -0500

    Added some stuff for rxjs

--- src/main/scala/com/joescii/rjs/comet/CurrencyQuotes.scala
+++ src/main/scala/com/joescii/rjs/comet/CurrencyQuotes.scala
@@ -7,21 +7,41 @@ import net.liftmodules.ng._
 import net.liftweb.util.Schedule
 import net.liftweb.util.Helpers._
 import scala.concurrent.ExecutionContext.Implicits.global
+import java.util.Date
+
+case class CurrencyQuote(currency:String, quote:String)
 
 class CurrencyQuotes extends AngularActor {
+  val currencies =
+    "CLP" ::
+    "CNY" ::
+    "CRC" ::
+    "CUP" ::
+    "EUR" ::
+    "GBP" ::
+    "INR" ::
+    "ILS" ::
+    "IRR" ::
+    "KGS" ::
+    "KRW" ::
+    "LAK" ::
+    "MNT" ::
+    "UAH" ::
+    "VND" ::
+      Nil
+
   override def lowPriority = {
-    case (currency:String, quote:String) => {
-      println(s"Quote: $currency == $quote")
-      scope.emit(currency, quote)
-    }
+    case q:CurrencyQuote => scope.emit("CurrencyQuote", q)
+    case t:Date => scope.emit("CurrentTime", t.toString)
   }
 
   def schedule:Unit = Schedule(() => {
-    exchange("EUR").foreach { quote =>
-      this ! ("EUR", quote)
-    }
+    currencies.foreach( currency =>
+      exchange(currency).foreach( quote => this ! CurrencyQuote(currency, quote) )
+    )
+    this ! new Date()
     schedule
-  }, 5.seconds)
+  }, 500 millis)
 
   schedule
 }
--- src/main/webapp/js/streams.js
+++ src/main/webapp/js/streams.js
@@ -1,11 +1,26 @@
 angular.module('Streams', [])
 
 .controller('StreamsSlide', ['$scope', function($scope){
-  $scope.$on('EUR', function(event, data){
-    console.log('yo??');
-    $scope.eur = data;
+  var events = function(topic) {
+    return Rx.Observable.create(function(observer){
+      return $scope.$on(topic, function(event, data){
+        observer.onNext(data);
+      })
+  })};
+  var quotes = events('CurrencyQuote');
+  var time = events('CurrentTime');
+
+  time.subscribe(function(data){
+    $scope.time = data;
   });
 
+  var currencyIs = function(currency) {
+    return function(quote) {
+      return quote.currency === currency;
+    }
+  };
+  var toQuote = function(data) { return data.quote };
+  quotes.where(currencyIs('EUR')).map(toQuote).forEach(function(eur){ $scope.eur = eur })
 }])
 
 ;
