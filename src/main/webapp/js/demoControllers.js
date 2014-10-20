5ff20062137f53ca66ba0049c42c870ca5afaedd
Joe Barnes <barnesjd@gmail.com>
2014-10-20 15:10:01 -0500

    StreamController stripped and ready for presentation

--- src/main/webapp/js/demoControllers.js
+++ src/main/webapp/js/demoControllers.js
@@ -61,28 +61,22 @@ angular.module('DemoControllers', ['DemoServices', 'rx'])
   }
 }])
 
-.factory('StreamsController', ['PromisesController', 'observeOnScope', 'promiseHttp', function(ctrl, observe, http){
+.factory('StreamsController', ['PromisesController', 'observeOnScope', 'promiseHttp',
+  function(ctrl, observe, http){
   var construct = function($scope){
     ctrl.forScope($scope);
-    observe($scope, 'symSearch')
-      .map(function(e){return e.newValue})
-      .filter(function(v){return v})
-      .throttle(1000)
-      .distinctUntilChanged()
-      .subscribe(function(v){
-        http.get("/suggest/"+encodeURIComponent(v))
-          .then(function(suggestions) {
-            $scope.symChoices = suggestions;
-          })
-          .catch(function(err) {
-            console.log('well, damn');
-            console.log(err);
-          })
-      });
 
+    // Click handler
     $scope.selectSuggestion = function(index) {
       $scope.symbol = $scope.symChoices[index].symbol;
     };
+
+
+
+    // /suggest/<query>
+
+    
+
   };
 
   return {
