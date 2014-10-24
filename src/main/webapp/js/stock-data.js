ac3e398459a0cfb54f5d3a2b53cde105087d9c86
Joe Barnes <barnesjd@gmail.com>
2014-10-24 09:07:49 -0500

    Refactored to have my stock data analysis functions in a separate file

---
+++ src/main/webapp/js/stock-data.js
@@ -0,0 +1,52 @@
+const StockData = {
+  parse: function(csv) {
+    const lines = csv.split(/\n/);
+    const data = [];
+    for(var i = lines.length-2; i > 0; i--){
+      var arr = lines[i].split(/,/);
+      var datum = [
+        new Date(arr[0]), // date:
+//      parseFloat(arr[1]), // open:
+//      parseFloat(arr[2]), // high:
+//      parseFloat(arr[3]), // low:
+        parseFloat(arr[4]) // close:
+//      parseFloat(arr[5]), // volume:
+//      parseFloat(arr[6]) // adjClose:
+      ];
+      data.push(datum);
+    }
+    return {
+      data: data,
+      labels: ["Date", "Price"]
+    };
+  },
+
+  addCalculation: function(data, calc, label) {
+    const values = data.data.map(function(datum) { return datum[1] });
+    const calculated = calc(values);
+    const dataArr = [];
+    for(var i = 0; i < data.data.length; i++) {
+      dataArr.push(data.data[i].concat(calculated[i]))
+    }
+    return {
+      data: dataArr,
+      labels: data.labels.concat(label)
+    }
+  },
+
+  addSmavg: function(data) {
+    const fn = function(values) { return TA.SMAverage(values, 100) };
+    return StockData.addCalculation(data, fn, "SMA")
+  },
+
+  addEmavg: function(data) {
+    const fn = function(values) { return TA.EMAverage(values, 100) };
+    return StockData.addCalculation(data, fn, "EMA")
+  },
+
+  addLinearReg: function(data) {
+    const fn = function(values) { return TA.LinearReg(values, 20) };
+    return StockData.addCalculation(data, fn, "LinearReg");
+  }
+
+};
--- src/main/webapp/js/worker.js
+++ src/main/webapp/js/worker.js
@@ -1,4 +1,4 @@
-importScripts('/js/q.js', '/js/js-TA.js');
+importScripts('/js/q.js', '/js/js-TA.js', '/js/stock-data.js');
 
 const httpGet = function(url) {
   const defer = Q.defer();
@@ -17,56 +17,6 @@ const httpGet = function(url) {
   return defer.promise;
 };
 
-const parse = function(csv) {
-  const lines = csv.split(/\n/);
-  const data = [];
-  for(var i = lines.length-2; i > 0; i--){
-    var arr = lines[i].split(/,/);
-    var datum = [
-      new Date(arr[0]), // date:
-//      parseFloat(arr[1]), // open:
-//      parseFloat(arr[2]), // high:
-//      parseFloat(arr[3]), // low:
-      parseFloat(arr[4]) // close:
-//      parseFloat(arr[5]), // volume:
-//      parseFloat(arr[6]) // adjClose:
-    ];
-    data.push(datum);
-  }
-  return {
-    data: data,
-    labels: ["Date", "Price"]
-  };
-};
-
-const addCalculation = function(data, calc, label) {
-  const values = data.data.map(function(datum) { return datum[1] });
-  const calculated = calc(values);
-  const dataArr = [];
-  for(var i = 0; i < data.data.length; i++) {
-    dataArr.push(data.data[i].concat(calculated[i]))
-  }
-  return {
-    data: dataArr,
-    labels: data.labels.concat(label)
-  }
-};
-
-const addSmavg = function(data) {
-  const fn = function(values) { return TA.SMAverage(values, 100) };
-  return addCalculation(data, fn, "SMA")
-};
-
-const addEmavg = function(data) {
-  const fn = function(values) { return TA.EMAverage(values, 100) };
-  return addCalculation(data, fn, "EMA")
-};
-
-const addLinearReg = function(data) {
-  const fn = function(values) { return TA.LinearReg(values, 20) };
-  return addCalculation(data, fn, "LinearReg");
-};
-
 const postData = function(data) {
   postMessage(data);
   return data;
@@ -80,10 +30,10 @@ const log = function(obj){
 onmessage = function(event) {
   if(event.data) {
     httpGet('/history/'+event.data)
-      .then(parse)
-      .then(addSmavg)
-      .then(addEmavg)
-      .then(addLinearReg)
+      .then(StockData.parse)
+      .then(StockData.addSmavg)
+      .then(StockData.addEmavg)
+      .then(StockData.addLinearReg)
   //  .then(log)
       .then(postData)
       .catch(function (status) {
