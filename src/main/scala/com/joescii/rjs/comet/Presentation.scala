c9fdf206ac8151f3541b6ffdee43d4a860f989a5
Joe Barnes <barnesjd@gmail.com>
2014-10-27 22:11:19 -0500

    Presenter now controls clients

---
+++ src/main/scala/com/joescii/rjs/comet/Presentation.scala
@@ -0,0 +1,28 @@
+package com.joescii.rjs.comet
+
+import net.liftweb.actor._
+import net.liftweb.http._
+import js._
+import JsCmds._
+import JE._
+
+import scala.xml.NodeSeq
+
+object Presentation extends LiftActor with ListenerManager {
+  var currentStep:String = "title"
+
+  def createUpdate = currentStep
+
+  override def lowPriority = {
+    case id:String => currentStep = id; updateListeners()
+  }
+}
+
+class PresentationComet extends CometActor with CometListener {
+  override def render = NodeSeq.Empty
+  override def registerWith = Presentation
+
+  override def lowPriority = {
+    case id:String => partialUpdate(Call("window.impress().goto", id))
+  }
+}
--- src/main/scala/com/joescii/rjs/snippet/Presenter.scala
+++ src/main/scala/com/joescii/rjs/snippet/Presenter.scala
@@ -6,6 +6,7 @@ import JsCmds._
 import JE._
 import net.liftweb.util.Helpers._
 import net.liftweb.util.ClearNodes
+import com.joescii.rjs.comet.Presentation
 
 /**
  * Created by jbarnes on 10/27/2014.
@@ -14,8 +15,7 @@ object Presenter {
   def render = if(Host.isLocal) "*" #> Script(
     JsCrVar("PresenterApi", JsObj()) &
     SetExp(JsVar("PresenterApi.goto"), AnonFunc("id", SHtml.ajaxCall(JsVar("id"), {(id:String) =>
-      println(s"goto($id)")
-      Call("console.log", id)
+      Presentation ! id
     })))
   ) else ClearNodes
 }
